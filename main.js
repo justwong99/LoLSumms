34f70ad2552d64931178dbff6e54abb3cef524d3
justwong99 <mashimar@live.ca>
2017-08-11 00:58:04 -0400

    can pass in summoner spell times

--- main.js
+++ main.js
@@ -1,3 +1,31 @@
+var tempid = "";
+var tempspellid = "";
+var summs = new Array();
+summs['spell-Barrier'] = 180;
+summs['spell-Clarity'] = 240;
+summs['spell-Cleanse'] = 210;
+summs['spell-Exhaust'] = 210;
+summs['spell-Flash'] = 300;
+summs['spell-Ghost'] = 180;
+summs['spell-Heal'] = 240;
+summs['spell-Ignite'] = 210;
+summs['spell-Mark'] = 80;
+summs['spell-Smite'] = 75;
+summs['spell-Teleport'] = 300;
+
+var sumSwitch = new Array();
+sumSwitch['spell-Barrier'] = "Barrier";
+sumSwitch['spell-Clarity'] = "Clarity";
+sumSwitch['spell-Cleanse'] = "Cleanse";
+sumSwitch['spell-Exhaust'] = "Exhaust";
+sumSwitch['spell-Flash'] = "Flash";
+sumSwitch['spell-Ghost'] = "Ghost";
+sumSwitch['spell-Heal'] = "Heal";
+sumSwitch['spell-Ignite'] = "Ignite";
+sumSwitch['spell-Mark'] = "Mark";
+sumSwitch['spell-Smite'] = "Smite";
+sumSwitch['spell-Teleport'] = "Teleport";
+
 function search(){
 	var x = document.getElementById('summoner').value;
 	alert(x);
@@ -9,13 +37,13 @@ function handle(e){
             search();
         }
     }
-var tempid = "";
+
 function change(champ){	
 	$("#" + tempid).css("background-image", "url(Champions/" + champ + ".png)");
 	
 }
 function changeSpell(spell){ 
-  $("#" + tempid).css("background-image", "url(Spells/" + spell + ".png)");
+  $("#" + tempspellid).css("background-image", "url(Spells/" + spell + ".png)");
   
 }
 function showDropdown(){
@@ -40,7 +68,37 @@ function hideBar(){
       }
     }
 }
+var spellcooldown;
+  var timer;
+function getCooldown(){
+  var tempcooldown = $("#" + tempspellid).prop("classList");
+  spellcooldown = summs[tempcooldown[1]];
+}
+function startCount(){
+  if(!timer){
+    var tempImg = $("#" + tempspellid).prop("classList");
+    $("#" + tempspellid).css("background-image", "url(Spells/" + sumSwitch[tempImg[1]] +"2.png)");
+   document.getElementById(tempspellid).innerHTML = spellcooldown;
+  timer = setInterval("countdown()", 1000);
+}
+else{
+  var tempImg = $("#" + tempspellid).prop("classList");
+    $("#" + tempspellid).css("background-image", "url(Spells/" + sumSwitch[tempImg[1]] +".png)");
+  clearInterval(timer);
+    timer = false;
 
+  document.getElementById(tempspellid).style.opacity = "1";
+  document.getElementById(tempspellid).innerHTML = "";
+}
+}
+function countdown(){
+   spellcooldown--;
+   document.getElementById(tempspellid).innerHTML = spellcooldown;
+   if (spellcooldown == 0){
+    clearInterval(timer);
+    timer = false;
+   }
+}
 
 
 
@@ -72,10 +130,30 @@ window.onclick = function(event) {
   }
 }
 
-function checkspell(){
-if (spellToggle == true){
+function removeSpell(tempspellid){
+  $("#" + tempspellid).removeClass (function (index, className) {
+    return (className.match (/(^|\s)spell-\S+/g) || []).join(' ');
+});
+}
+
+$( ".champion" ).click(function() {
+  tempid = (this.id);
+  showDropdown();
+});
+$( ".spell" ).click(function() {
+  tempspellid = (this.id);
+  if (spellToggle == true){
   showSpellDropdown();
 }
+ else{
+  getCooldown();
+  startCount();
+ }
+});
 
-
-}
+$( ".spell-choice" ).click(function() {
+  changeSpell(this.id);
+  var secondClass = "spell-" + (this.id);
+  removeSpell(tempspellid);
+  $("#" + tempspellid).addClass(secondClass);
+});
