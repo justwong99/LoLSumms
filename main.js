b0bb5ddbe9b35bda17ec051e51fa1630183a0234
justwong99 <mashimar@live.ca>
2017-09-26 22:34:08 -0400

    xd

--- main.js
+++ main.js
@@ -152,7 +152,7 @@ function search(){
   var x = document.getElementById('summoner').value;
   var y = document.getElementById('regionselect').value;
     $.ajax({
-      url:'https://lolsumms-server-lolsumms-server.193b.starter-ca-central-1.openshiftapps.com/info',
+      url:'lolsumms-server-lolsumms-server.193b.starter-ca-central-1.openshiftapps.com/info',
       data: {name: x, region: y}, // pass in summoner name and region for api call
       dataType: "json",
       crossDomain: true,
